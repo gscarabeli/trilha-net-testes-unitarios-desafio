6f1c5ec56a7ee621865e67a3fd3aa1a09aed0283
gscarabeli <gustavocorreiascarabeli@gmail.com>
2024-04-23 20:46:14 -0300

    Resolução Editada

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -37,8 +37,6 @@ public class ValidacoesListaTests
     [Fact]
     public void NaoDeveConterONumero10NaLista()
     {
-        //TODO: Implementar método de teste
-
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
         var numeroParaProcurar = 10;
@@ -50,12 +48,9 @@ public class ValidacoesListaTests
         Assert.False(resultado);
     }
 
-    //TODO: Corrigir a anotação [Fact]
     [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
-        //TODO: Implementar método de teste
-
         // Arrange
         var lista = new List<int> { 5, 7, 8, 9 };
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
@@ -70,8 +65,6 @@ public class ValidacoesListaTests
     [Fact]
     public void DeveRetornar9ComoMaiorNumeroDaLista()
     {
-        //TODO: Implementar método de teste
-
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
         var numeroEsperado = 9;
@@ -80,15 +73,12 @@ public class ValidacoesListaTests
         var resultado = _validacoes.RetornarMaiorNumeroLista(lista);
 
         // Assert
-        //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
         Assert.Equal(numeroEsperado, resultado);
     }
 
     [Fact]
     public void DeveRetornarOitoNegativoComoMenorNumeroDaLista()
     {
-        //TODO: Implementar método de teste
-
         // Arrange
         var lista = new List<int> { 5, -1, -8, 9 };
         var numeroEsperado = -8;
@@ -97,7 +87,6 @@ public class ValidacoesListaTests
         var resultado = _validacoes.RetornarMenorNumeroLista(lista);
 
         // Assert
-        //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
         Assert.Equal(numeroEsperado, resultado);
     }
 }
